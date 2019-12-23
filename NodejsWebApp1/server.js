c08255404475ee0bf74e9d6cb003503c0d4edb69
kirin <kirin.chen1001@gmail.com>
2019-12-23 14:28:12 +0800

    done the project

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -2,36 +2,19 @@
 const express = require('express');
 const app = express();
 var AccessLogger = require('./AccessLogger');
-var http = require('http');
+var https = require('https');
 var port = process.env.PORT || 1337;
 var ipAccesses = new AccessLogger(10, 60 * 1000);
 var idAccesses = new AccessLogger(5, 60 * 1000);
-var url = 'http://hungjie19.github.io/myapi/test.json';
+var url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
 
 app.get('/', (req, res) => {
-    console.log(req.query)
-    res.writeHead(403, { 'Content-Type': 'application/json' });
-    res.end("ID is null");
-});
-app.listen(port);
-/*
-http.createServer(function (req, res) {
-
-    if (req.query == {}) {
-        res.writeHead(403, { 'Content-Type': 'application/json' });
-        res.end("ID is null");
-        return;
-    }
-
     var id = req.query.id;
     var ipAr = ipAccesses.check(req.connection.remoteAddress);
-    var idAr = ipAccesses.check(req.connection.remoteAddress);
-
-
-
+    var idAr = idAccesses.check(id);
 
     if (ipAr.ok && idAr.ok) {
-        http.get(url, function (response) {
+        https.get(url, function (response) {
             var data = '';
             // response event 'data' 當 data 陸續接收的時候，用一個變數累加它。
             response.on('data', function (chunk) {
@@ -48,7 +31,7 @@ http.createServer(function (req, res) {
             console.log("error: ", e);
             res.writeHead(500, { 'Content-Type': 'application/json' });
             res.end("");
-        });        
+        });
     } else {
         res.writeHead(403, { 'Content-Type': 'application/json' });
         var msg = {
@@ -58,4 +41,6 @@ http.createServer(function (req, res) {
         res.end(JSON.stringify(msg));
     }
 
-}).listen(port);*/
+});
+app.listen(port);
+
